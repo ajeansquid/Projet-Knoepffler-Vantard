669908ea7481d6d3b4eeb9b0c3ca6c50ceb8206c
ajeansquid <lp.knoepffler@gmail.com>
2024-11-13 13:46:01 +0100

    test+fin round

--- Classe.py
+++ Classe.py
@@ -6,38 +6,35 @@ class User :
     def jouer (self,num_carte,actual_round):
         actual_round.jouerCartes(self.id,num_carte)
 
-
 class Round :
     def __init__(self,num,nb_joueur):
         self.num = num
         self.nb_subRound = 0
         self.subRounds = []
-        plateau = [] # liste de cartes jouées
+        self.plateau = [] # liste de cartes jouées
         self.nb_joueur = nb_joueur
-        self.prompt =""
+        self.prompt= ""
         
-    def prompt(self, prompt):
+    def defPrompt(self, prompt):
         self.prompt = prompt
         
     def jouerCartes(self, id, numCarte):
         self.plateau.append([id,numCarte])
         if len(self.plateau) == self.nb_joueur:
+            print("Round fini")
             self.finirRound()
 
     def finirRound(self):
-        cache = self.plateau[0[1]]
+        cache = self.plateau[0]
         door = True
         for elements in self.plateau:
-            if elements[1]!= cache:
+            if elements[1]!= cache[1]:
                 door = False
         if door == False:
+            print("Nouveau rounds -> subround crée")
             self.subRounds.append(self)
             self.nb_subRound += 1
             self.plateau = []
-        
-            
-        
-    
 
 class Game :
     def __init__(self, id, path, prompts):
@@ -47,15 +44,23 @@ class Game :
         self.rounds = []
         self.prompts = prompts
         
+    def add_user(self, name):
+        self.joueurs.append(User(name,len(self.joueurs)))
+    
     def start_round(self):
         round = Round(len(self.rounds)+1,len(self.joueurs))
         self.rounds.append(round)
-        round.prompt(self.prompt)
+        round.defPrompt(self.prompts[len(self.rounds)-1])
         
-        while len(round.subRounds) < 5 or round.plateau != []: # 5 subrounds (illimité en mode strict)
+        doorv2=True
+        while len(round.subRounds) < 5 and doorv2: # 5 subrounds (illimité en mode strict)
             for i in range(len(self.joueurs)):
-                self.joueurs[i].jouer(i,round)
+                x = input("carte du joueur : "+self.joueurs[i].name)
+                self.joueurs[i].jouer(x,round)
+            if round.plateau != []:
+                doorv2 = False
         
+        print("Round fini")
         
         
 
---
+++ test.py
@@ -0,0 +1,9 @@
+import Classe as cl
+
+Gamming = cl.Game(1, "path", ["testv1?","testv2 ?"])
+Gamming.add_user("test1")
+Gamming.add_user("test2")
+Gamming.add_user("test3")
+Gamming.add_user("test4")
+
+Gamming.start_round()
